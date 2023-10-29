3a9d5284307cecf78bcc9fd629b6875ccd63bf55
Meirambe <vip.sultangaliev@bk.ru>
2023-10-29 16:35:33 +0600

    coment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        string result = PrintNaturalNumbers(N);
+        Console.WriteLine(result);
+    }
+
+    static string PrintNaturalNumbers(int N)
+    {
+        if (N == 1)
+        {
+            return "1";
+        }
+        else
+        {
+            return N + ", " + PrintNaturalNumbers(N - 1);
+        }
+    }
+}
